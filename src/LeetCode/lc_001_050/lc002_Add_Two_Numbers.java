504de67f6ac9c2499d9d11912bec8b99c291fbb1
Yen-Tin Liu <Yen-Tin.Liu@ipsoft.com>
2016-03-20 13:52:15 -0400

    fight for new job

---
+++ src/LeetCode/lc_001_050/lc002_Add_Two_Numbers.java
@@ -0,0 +1,7 @@
+package LeetCode.lc_001_050;
+
+/**
+ * Created by ytliu on 3/12/16.
+ */
+public class lc002_Add_Two_Numbers {
+}
---
+++ src/LeetCode/lc_001_050/lc003_Longest_Substring_Without_Repeating_Characters.java
@@ -0,0 +1,7 @@
+package LeetCode.lc_001_050;
+
+/**
+ * Created by ytliu on 3/12/16.
+ */
+public class lc003_Longest_Substring_Without_Repeating_Characters {
+}
